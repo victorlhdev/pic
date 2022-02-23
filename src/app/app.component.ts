707ee7d8376ab9bd6ec853e9b80ac35c67beff6e
victorlhdev <victorlhdev@gmail.com>
2022-02-23 17:56:38 -0300

    v1.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { PhotoService } from './photos/photo/photo.service';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
 
   photos: Photo[] = [];
 
-  constructor(http:HttpClient){
+  constructor(PhotoService: PhotoService){
 
-    http
-    .get<Photo[]>('http://localhost:3000/flavio/photos')
-    .subscribe(photos => this.photos = photos);
+    PhotoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos)
+
   }
 
-  }
+  };
 
   interface Photo{
     url: string
     description: string
-}
+  };
+
+
