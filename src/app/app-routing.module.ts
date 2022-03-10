0bc6a13e108ccb01bf233b5f96ba0469a12694e1
victorlhdev <victorlhdev@gmail.com>
2022-03-09 23:57:41 -0300

    v1.8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
